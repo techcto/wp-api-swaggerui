72af44e3f33e506678d3047f8a677df441834bb3
Luis Sanchez <luis.sanchez@solodev.com>
2024-09-04 11:15:03 -0400

    update package

--- cmd.sh
+++ cmd.sh
@@ -10,6 +10,18 @@ tag(){
 bundle(){
     npm install
     npm run build
+
+    FILES="assets bin template tests swaggerauth.php swaggerbag.php swaggersetting.php swaggertemplate.php wp-api-swaggerui.php"
+
+    # On WSL uname typically returns Linux
+    if [[ "$(uname -s)" == "Linux" ]]; then
+        7z a -r output/swagger.zip $FILES
+    else
+        zip a -r output/swagger.zip $FILES
+    fi
+
+    rm -f ../../devops/ami/wordpress/bin/swagger.zip
+    cp output/swagger.zip ../../devops/ami/wordpress/bin/swagger.zip
 }
 
 $*
